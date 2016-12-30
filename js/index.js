3e7adef19122f247e73a8369df7a760de6f338d0
Peter Dulworth <pdulworth97@gmail.com>
2016-12-30 16:48:03 -0600

    fix?

--- js/index.js
+++ js/index.js
@@ -9,13 +9,13 @@
             this.$downArrowWrapper = $("#down-arrow");
             this.$navTexas = $("#nav-texas");
             this.$navCanada = $("#nav-canada");
-            this.$navCalifornia = $("#nav-california");
+            //this.$navCalifornia = $("#nav-california");
             this.$navGalveston = $("#nav-galveston");
             this.$navCrossfire = $("#nav-crossfire");
             this.$downArrow = $("#down-arrow");
             this.$discoTexas = $('#disco-texas');
             this.$discoCanada = $('#disco-canada');
-            this.$discoCalifornia = $('#disco-california');
+            //this.$discoCalifornia = $('#disco-california');
             this.$discoGalveston = $('#disco-galveston');
             this.$discoCrossfire = $('#disco-crossfire');
             this.$doc = $(document);
@@ -25,7 +25,7 @@
             this.$navTexas.on('click', {scrollTop: $( window ).height() + 1, duration: 500}, this.scrollToVal.bind(this));
             this.$navCanada.on('click', {scrollTop: $("#disco-canada").offset().top, duration: 500}, this.scrollToVal.bind(this));
             //this.$navCalifornia.on('click', {scrollTop: $("#disco-california").offset().top, duration: 500}, this.scrollToVal.bind(this));
-            this.$navGalveston.on('click', {scrollTop: $("disco-galveston").offset().top, duration: 500}, this.scrollToVal.bind(this));
+            this.$navGalveston.on('click', {scrollTop: $("#disco-galveston").offset().top, duration: 500}, this.scrollToVal.bind(this));
             this.$navCrossfire.on('click', {scrollTop: $("#disco-crossfire").offset().top, duration: 500}, this.scrollToVal.bind(this));
             this.$discoTexas.on('click', {page: 'texas.html'}, this.redirect.bind(this));
             this.$discoCanada.on('click', {page: 'canada.html'}, this.redirect.bind(this));
